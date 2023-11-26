09bdc2603f0b15cd3ef5bf481051befb9765716b
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-11-26 11:07:35 +0100

    Przesłano rozwiązanie problemu ReverseInteger

---
+++ ReverseInteger/ReverseInteger.cs
@@ -0,0 +1,48 @@
+﻿namespace ReverseInteger;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: x = 123
+        // Output: 321
+        // Example 2:
+        //
+        // Input: x = -123
+        // Output: -321
+        // Example 3:
+        //
+        // Input: x = 120
+        // Output: 21
+
+        int i1 = 123;
+        int i2 = -123;
+        int i3 = 120;
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.Reverse(i1));
+        Console.WriteLine(solution.Reverse(i2));
+        Console.WriteLine(solution.Reverse(i3));
+
+    }
+    
+    public int Reverse(int x)
+    {
+        char[] xChrArr = x.ToString().ToCharArray();
+        char sign = '+';
+        if (xChrArr[0] == '-')
+        {
+            xChrArr = xChrArr.Skip(1).ToArray();
+            sign = '-';
+        }
+
+        Array.Reverse(xChrArr);
+
+        string outStr = sign + new string(xChrArr);
+        if (!int.TryParse(outStr, out int outInt)) return 0;
+        return outInt;
+    }
+}
