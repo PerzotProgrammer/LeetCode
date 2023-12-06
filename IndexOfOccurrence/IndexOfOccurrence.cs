3c8099454bf82239179105224376a08aeeb6b75f
PerzotProgrammer <bartoszperz2007@gmail.com>
2023-12-06 13:38:02 +0100

    Przesłano rozwiązanie problemu IndexOfOccurrence

---
+++ IndexOfOccurrence/IndexOfOccurrence.cs
@@ -0,0 +1,36 @@
+﻿namespace IndexOfOccurrence;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: haystack = "sadbutsad", needle = "sad"
+        // Output: 0
+        // Explanation: "sad" occurs at index 0 and 6.
+        //     The first occurrence is at index 0, so we return 0.
+        //     Example 2:
+        //
+        // Input: haystack = "leetcode", needle = "leeto"
+        // Output: -1
+        // Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+        string s1a = "sadbutsad";
+        string s1b = "sad";
+        string s2a = "leetcode";
+        string s2b = "leeto";
+
+        Solution solution = new();
+        
+        Console.WriteLine(solution.StrStr(s1a, s1b));
+        Console.WriteLine(solution.StrStr(s2a, s2b));
+    }
+
+    public int StrStr(string haystack, string needle)
+    {
+        if (!haystack.Contains(needle)) return -1;
+        return haystack.IndexOf(needle, StringComparison.Ordinal);
+    }
+}
+
