befe3c83d8880ffa954082a220b57015a24c8a85
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-02-12 22:18:54 +0100

    Przesłano rozwiązanie problemu MajorityElement

---
+++ MajorityElement/MajorityElement.cs
@@ -0,0 +1,48 @@
+﻿namespace MajorityElement;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+
+        // Input: nums = [3,2,3]
+        // Output: 3
+
+        // Example 2:
+
+        // Input: nums = [2,2,1,1,1,2,2]
+        // Output: 2
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.MajorityElement([3, 2, 3]));
+        Console.WriteLine(solution.MajorityElement([2, 2, 1, 1, 1, 2, 2]));
+    }
+
+    public int MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> freq = new();
+
+        foreach (int num in nums)
+        {
+            if (!freq.TryAdd(num, 1))
+            {
+                freq[num]++;
+            }
+        }
+
+        int maxElement = 0;
+        int maxValue = 0;
+        foreach (KeyValuePair<int, int> pair in freq)
+        {
+            if (pair.Value > maxValue)
+            {
+                maxElement = pair.Key;
+                maxValue = pair.Value;
+            }
+        }
+
+        return maxElement;
+    }
+}
