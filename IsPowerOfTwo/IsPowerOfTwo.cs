c257d3421993b6c058cfe3dbd6bf0193a022273a
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-02-19 09:03:44 +0100

    Przesłano rozwiązanie problemu IsPowerOfTwo

---
+++ IsPowerOfTwo/IsPowerOfTwo.cs
@@ -0,0 +1,45 @@
+﻿namespace IsPowerOfTwo;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: n = 1
+        // Output: true
+        // Explanation: 2^0 = 1
+
+        // Example 2:
+        //
+        // Input: n = 16
+        // Output: true
+        // Explanation: 2^4 = 16
+
+        // Example 3:
+        //
+        // Input: n = 3
+        // Output: false
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.IsPowerOfTwo(1));
+        Console.WriteLine(solution.IsPowerOfTwo(16));
+        Console.WriteLine(solution.IsPowerOfTwo(3));
+        Console.WriteLine(solution.IsPowerOfTwo(8));
+    }
+
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n == 1) return true;
+        if (n < 1) return false;
+
+        while (n > 1)
+        {
+            if (n % 2 != 0) return false;
+            n /= 2;
+        }
+
+        return true;
+    }
+}
