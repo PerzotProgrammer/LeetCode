19e51ba07bf22d1a89be45a59d994d9b2c6ea542
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-03-02 10:48:57 +0100

    Przesłano rozwiązanie problemu SortedSquares

---
+++ SortedSquares/SortedSquares.cs
@@ -0,0 +1,41 @@
+﻿namespace SortedSquares;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: nums = [-4,-1,0,3,10]
+        // Output: [0,1,9,16,100]
+        // Explanation: After squaring, the array becomes [16,1,0,9,100].
+        // After sorting, it becomes [0,1,9,16,100].
+
+        // Example 2:
+        //
+        // Input: nums = [-7,-3,2,3,11]
+        // Output: [4,9,9,49,121]
+
+        Solution solution = new();
+
+        int[] a1 = solution.SortedSquares([-4,-1,0,3,10]);
+        int[] a2 = solution.SortedSquares([-7,-3,2,3,11]);
+        
+        PrintArray(a1);
+        PrintArray(a2);
+    }
+
+    public int[] SortedSquares(int[] nums)
+    {
+        int[] output = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++) output[i] = nums[i] * nums[i];
+        Array.Sort(output);
+        return output;
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int i in arr) Console.Write($"{i}, ");
+        Console.WriteLine();
+    }
+}
