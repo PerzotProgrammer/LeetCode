493278880a21b102e9c3a41eb4799d410ec0922a
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-03-15 21:37:32 +0100

    Przesłano rozwiązanie problemu ProductExceptSelf

---
+++ ProductExceptSelf/ProductExceptSelf.cs
@@ -0,0 +1,50 @@
+﻿namespace ProductExceptSelf;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: nums = [1,2,3,4]
+        // Output: [24,12,8,6]
+
+        // Example 2:
+        //
+        // Input: nums = [-1,1,0,-3,3]
+        // Output: [0,0,9,0,0]
+
+        Solution solution = new();
+
+        PrintArray(solution.ProductExceptSelf([1, 2, 3, 4]));
+        PrintArray(solution.ProductExceptSelf([-1,1,0,-3,3]));
+    }
+
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int n = nums.Length;
+        int[] answer = new int[n];
+
+        int left = 1;
+        for (int i = 0; i < n; i++)
+        {
+            answer[i] = left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            answer[i] *= right;
+            right *= nums[i];
+        }
+
+        return answer;
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int i in arr) Console.Write($"{i}, ");
+        Console.WriteLine();
+    }
+}
