41f5c25f652697284bcc977166acf7bd95b65369
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-06-01 22:47:50 +0200

    Przesłano rozwiązanie problemu ScoreOfString

---
+++ ScoreOfString/ScoreOfString.cs
@@ -0,0 +1,36 @@
+﻿namespace ScoreOfString;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: s = "hello"
+        // Output: 13
+        // Explanation: The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+        //
+        // Example 2:
+        // Input: s = "zaz"
+        // Output: 50
+        // Explanation: The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.ScoreOfString("hello"));
+        Console.WriteLine(solution.ScoreOfString("zaz"));
+    }
+
+    public int ScoreOfString(string s)
+    {
+        int sum = 0;
+        int prev = s[0];
+        for (int i = 1; i < s.Length; i++)
+        {
+            sum += Math.Abs(prev - s[i]);
+            prev = s[i];
+        }
+
+        return sum;
+    }
+}
