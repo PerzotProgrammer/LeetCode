8439e613c82ffc0ec8c7c47f75cae3f00e602514
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-06-02 23:52:56 +0200

    Przesłano rozwiązanie problemu ReverseString

---
+++ ReverseString/ReverseString.cs
@@ -0,0 +1,48 @@
+﻿namespace ReverseString;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: s = ["h","e","l","l","o"]
+        // Output: ["o","l","l","e","h"]
+
+        // Example 2:
+        //
+        // Input: s = ["H","a","n","n","a","h"]
+        // Output: ["h","a","n","n","a","H"]
+
+
+        char[] s1 = ['h', 'e', 'l', 'l', 'o'];
+        char[] s2 = ['H', 'a', 'n', 'n', 'a', 'h'];
+
+        Solution solution = new();
+
+        solution.ReverseString(s1);
+        solution.ReverseString(s2);
+
+        PrintArray(s1);
+        PrintArray(s2);
+    }
+
+    public void ReverseString(char[] s)
+    {
+        int low = 0;
+        int high = s.Length - 1;
+
+        while (low < high)
+        {
+            (s[low], s[high]) = (s[high], s[low]);
+            low++;
+            high--;
+        }
+    }
+
+    static void PrintArray(char[] arr)
+    {
+        foreach (char c in arr) Console.Write($"{c},");
+        Console.WriteLine();
+    }
+}
