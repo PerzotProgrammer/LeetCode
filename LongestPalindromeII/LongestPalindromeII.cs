dbd63591e55012fdaa133911c177ab7fd76d0378
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-06-05 00:46:07 +0200

    Przesłano rozwiązanie problemu LongestPalindromeII

---
+++ LongestPalindromeII/LongestPalindromeII.cs
@@ -0,0 +1,49 @@
+﻿namespace LongestPalindromeII;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: s = "abccccdd"
+        // Output: 7
+        // Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+        
+        // Example 2:
+        //
+        // Input: s = "a"
+        // Output: 1
+        // Explanation: The longest palindrome that can be built is "a", whose length is 1.
+
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.LongestPalindrome("abccccdd"));
+        Console.WriteLine(solution.LongestPalindrome("a"));
+    }
+
+    public int LongestPalindrome(string s)
+    {
+        int[] charCount = new int[128];
+
+        foreach (char c in s) charCount[c]++;
+
+        int length = 0;
+        bool hasOddCount = false;
+
+        foreach (int count in charCount)
+        {
+            if (count % 2 == 0) length += count;
+            else
+            {
+                length += count - 1;
+                hasOddCount = true;
+            }
+        }
+
+        if (hasOddCount) length += 1;
+
+        return length;
+    }
+}
