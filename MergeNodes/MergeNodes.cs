133596c6d07e69eeea7d7178a817831ea2a9903c
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-07-05 01:46:09 +0200

    Przesłano rozwiązanie problemu MergeNodes

---
+++ MergeNodes/MergeNodes.cs
@@ -0,0 +1,99 @@
+﻿namespace MergeNodes;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: head = [0,3,1,0,4,5,2,0]
+        // Output: [4,11]
+        // Explanation: 
+        // The above figure represents the given linked list. The modified list contains
+        //     - The sum of the nodes marked in green: 3 + 1 = 4.
+        // - The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+
+        // Example 2:
+        //
+        // Input: head = [0,1,0,3,0,2,2,0]
+        // Output: [1,3,4]
+        // Explanation: 
+        // The above figure represents the given linked list. The modified list contains
+        //     - The sum of the nodes marked in green: 1 = 1.
+        // - The sum of the nodes marked in red: 3 = 3.
+        // - The sum of the nodes marked in yellow: 2 + 2 = 4.
+
+
+        Solution solution = new();
+
+        ListNode a01 = new ListNode(0, null);
+        ListNode a2 = new ListNode(2, a01);
+        ListNode a5 = new ListNode(5, a2);
+        ListNode a4 = new ListNode(4, a5);
+        ListNode a02 = new ListNode(0, a4);
+        ListNode a1 = new ListNode(1, a02);
+        ListNode a3 = new ListNode(3, a1);
+        ListNode a03 = new ListNode(0, a3);
+        
+        ListNode b01 = new ListNode(0, null);
+        ListNode b21 = new ListNode(2, b01);
+        ListNode b22 = new ListNode(2, b21);
+        ListNode b02 = new ListNode(0, b22);
+        ListNode b3 = new ListNode(3, b02);
+        ListNode b03 = new ListNode(0, b3);
+        ListNode b1 = new ListNode(1, b03);
+        ListNode b04 = new ListNode(0, b1);
+        
+        PrintList(solution.MergeNodes(a03));
+        PrintList(solution.MergeNodes(b04));
+    }
+
+    public ListNode MergeNodes(ListNode head)
+    {
+        ListNode currentNode = head;
+
+        while (currentNode != null && currentNode.next != null)
+        {
+            if (currentNode.next.val != 0)
+            {
+                currentNode.val += currentNode.next.val;
+                currentNode.next = currentNode.next.next;
+            }
+            else currentNode = currentNode.next;
+
+            if (currentNode.next != null && currentNode.next.val == 0 && currentNode.next.next == null)
+            {
+                currentNode.next = null!;
+            }
+        }
+
+        return head;
+    }
+
+    static void PrintList(ListNode root)
+    {
+        if (root == null) return;
+        ListNode current = root;
+        while (current.next != null)
+        {
+            Console.Write($"{current.val},");
+            current = current.next;
+        }
+
+        Console.Write($"{current.val}");
+        Console.WriteLine();
+    }
+}
+
+// Definition for singly-linked list.
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ MinDifference/MinDifference.cs
@@ -0,0 +1,67 @@
+﻿namespace MinDifference;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: nums = [5,3,2,4]
+        // Output: 0
+        // Explanation: We can make at most 3 moves.
+        // In the first move, change 2 to 3. nums becomes [5,3,3,4].
+        // In the second move, change 4 to 3. nums becomes [5,3,3,3].
+        // In the third move, change 5 to 3. nums becomes [3,3,3,3].
+        // After performing 3 moves, the difference between the minimum and maximum is 3 - 3 = 0.
+
+        // Example 2:
+        //
+        // Input: nums = [1,5,0,10,14]
+        // Output: 1
+        // Explanation: We can make at most 3 moves.
+        // In the first move, change 5 to 0. nums becomes [1,0,0,10,14].
+        // In the second move, change 10 to 0. nums becomes [1,0,0,0,14].
+        // In the third move, change 14 to 1. nums becomes [1,0,0,0,1].
+        // After performing 3 moves, the difference between the minimum and maximum is 1 - 0 = 1.
+        // It can be shown that there is no way to make the difference 0 in 3 moves.
+
+        // Example 3:
+        //
+        // Input: nums = [3,100,20]
+        // Output: 0
+        // Explanation: We can make at most 3 moves.
+        // In the first move, change 100 to 7. nums becomes [3,7,20].
+        // In the second move, change 20 to 7. nums becomes [3,7,7].
+        // In the third move, change 3 to 7. nums becomes [7,7,7].
+        // After performing 3 moves, the difference between the minimum and maximum is 7 - 7 = 0.
+
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.MinDifference([5, 3, 2, 4]));
+        Console.WriteLine(solution.MinDifference([1, 5, 0, 10, 14]));
+        Console.WriteLine(solution.MinDifference([3, 100, 20]));
+    }
+
+    public int MinDifference(int[] nums)
+    {
+        if (nums.Length <= 4) return 0;
+
+        int smallestDistance = int.MaxValue;
+        int[] sorted = nums;
+        int difIndex = 0;
+        Array.Sort(sorted);
+
+        for (int i = 0; i < 4; i++)
+        {
+            int difference = sorted[i + nums.Length - 4] - sorted[i];
+            if (difference < smallestDistance)
+            {
+                difIndex = i;
+                smallestDistance = difference;
+            }
+        }
+
+        return sorted[difIndex + nums.Length - 4] - sorted[difIndex];
+    }
+}
