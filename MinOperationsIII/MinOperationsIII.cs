025d3ddff26c63f82c6c7f11245d465b561dde3c
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-07-10 23:44:35 +0200

    Przesłano rozwiązanie problemu MinOperationsIII

---
+++ MinOperationsIII/MinOperationsIII.cs
@@ -0,0 +1,46 @@
+﻿namespace MinOperationsIII;
+
+class Solution
+{
+    static void Main()
+    {
+        // Example 1:
+        //
+        // Input: logs = ["d1/","d2/","../","d21/","./"]
+        // Output: 2
+        // Explanation: Use this change folder operation "../" 2 times and go back to the main folder.
+
+        // Example 2:
+        //
+        // Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+        // Output: 3
+
+        // Example 3:
+        //
+        // Input: logs = ["d1/","../","../","../"]
+        // Output: 0
+
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.MinOperations(["d1/", "d2/", "../", "d21/", "./"]));
+        Console.WriteLine(solution.MinOperations(["d1/", "d2/", "./", "d3/", "../", "d31/"]));
+        Console.WriteLine(solution.MinOperations(["d1/", "../", "../", "../"]));
+    }
+
+    public int MinOperations(string[] logs)
+    {
+        Stack<string> stack = new();
+
+        foreach (string log in logs)
+        {
+            if (log.Equals("../"))
+            {
+                if (stack.Count > 0) stack.Pop();
+            }
+            else if (!log.Equals("./")) stack.Push(log);
+        }
+
+        return stack.Count;
+    }
+}
