613d631332169977bcd6d82e353f0334f0262012
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-09-21 01:15:58 +0200

    Przesłano rozwiązanie problemu ShortestPalindrome

---
+++ ShortestPalindrome/ShortestPalindrome.cs
@@ -0,0 +1,45 @@
+﻿namespace ShortestPalindrome;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        // Example 1:
+        //
+        // Input: s = "aacecaaa"
+        // Output: "aaacecaaa"
+
+        // Example 2:
+        //
+        // Input: s = "abcd"
+        // Output: "dcbabcd"
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.ShortestPalindrome("aacecaaa"));
+        Console.WriteLine(solution.ShortestPalindrome("abcd"));
+    }
+
+    public string ShortestPalindrome(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return s;
+
+        int n = s.Length;
+        int end = n - 1;
+        int j = 0;
+
+        while (end >= 0)
+        {
+            if (s[end] == s[j]) j++;
+            end--;
+        }
+
+        if (j == n) return s;
+
+        string suffix = s.Substring(j);
+        char[] charArray = suffix.ToCharArray();
+        Array.Reverse(charArray);
+        string revSuffix = new string(charArray);
+        return revSuffix + ShortestPalindrome(s.Substring(0, j)) + suffix;
+    }
+}
