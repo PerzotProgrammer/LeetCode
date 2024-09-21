3f285aa72b054df3bd2fd5fa25a67a82733da06f
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-09-22 00:59:08 +0200

    Przesłano rozwiązanie problemu LexicalOrder

---
+++ LexicalOrder/LexicalOrder.cs
@@ -0,0 +1,48 @@
+﻿namespace LexicalOrder;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        // Example 1:
+        //
+        // Input: n = 13
+        // Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+        // Example 2:
+        //
+        // Input: n = 2
+        // Output: [1,2]
+
+
+        Solution solution = new();
+
+        PrintList(solution.LexicalOrder(13));
+        PrintList(solution.LexicalOrder(2));
+    }
+
+    public List<int> LexicalOrder(int n)
+    {
+        List<int> lexicographicalNumbers = new();
+        int currentNumber = 1;
+
+        for (int i = 0; i < n; ++i)
+        {
+            lexicographicalNumbers.Add(currentNumber);
+
+            if (currentNumber * 10 <= n) currentNumber *= 10;
+            else
+            {
+                while (currentNumber % 10 == 9 || currentNumber >= n) currentNumber /= 10;
+                currentNumber += 1;
+            }
+        }
+
+        return lexicographicalNumbers;
+    }
+
+    static void PrintList(List<int> list)
+    {
+        foreach (int i in list) Console.Write($"{i}, ");
+        Console.WriteLine();
+    }
+}
