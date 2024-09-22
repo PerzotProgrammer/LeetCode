45682467a7bdbec9014db24a605da1ab12a8775f
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-09-23 00:03:21 +0200

    Przesłano rozwiązanie problemu FindKthNumber

---
+++ FindKthNumber/FindKthNumber.cs
@@ -0,0 +1,62 @@
+﻿namespace FindKthNumber;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        // POMOC EDITORIALA
+        // Example 1:
+        //
+        // Input: n = 13, k = 2
+        // Output: 10
+        // Explanation: The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number is 10.
+
+        // Example 2:
+        //
+        // Input: n = 1, k = 1
+        // Output: 1
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.FindKthNumber(13, 2));
+        Console.WriteLine(solution.FindKthNumber(1, 1));
+    }
+
+    public int FindKthNumber(int n, int k)
+    {
+        int current = 1;
+        k--;
+
+        while (k > 0)
+        {
+            long steps = CountSteps(n, current, current + 1);
+
+            if (steps <= k)
+            {
+                current++;
+                k -= (int)steps;
+            }
+            else
+            {
+                current *= 10;
+                k--;
+            }
+        }
+
+        return current;
+    }
+
+    private long CountSteps(int n, long current, long next)
+    {
+        long steps = 0;
+
+        while (current <= n)
+        {
+            steps += Math.Min(n + 1, next) - current;
+            current *= 10;
+            next *= 10;
+        }
+
+        return steps;
+    }
+}
