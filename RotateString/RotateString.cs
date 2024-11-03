189e6d1224f7d46904bfac30e06d4a2d2a4baf21
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-11-03 23:27:57 +0100

    Przesłano rozwiązanie problemu RotateString

---
+++ RotateString/RotateString.cs
@@ -0,0 +1,28 @@
+﻿namespace RotateString;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        // Example 1:
+        //
+        // Input: s = "abcde", goal = "cdeab"
+        // Output: true
+
+        // Example 2:
+        //
+        // Input: s = "abcde", goal = "abced"
+        // Output: false
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.RotateString("abcde", "cdeab"));
+        Console.WriteLine(solution.RotateString("abcde", "abced"));
+    }
+
+    public bool RotateString(string s, string goal)
+    {
+        if (s.Length != goal.Length) return false;
+        return (s + s).Contains(goal);
+    }
+}
