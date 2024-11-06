6ab9eb9fcb288d363eb9342d7c7a2419dde99b37
PerzotProgrammer <bartoszperz2007@gmail.com>
2024-11-06 23:51:03 +0100

    Przesłano rozwiązanie problemu CanSortArray

---
+++ CanSortArray/CanSortArray.cs
@@ -0,0 +1,67 @@
+﻿namespace CanSortArray;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        // Example 1:
+        //
+        // Input: nums = [8,4,2,30,15]
+        // Output: true
+        // Explanation: Let's look at the binary representation of every element. The numbers 2, 4, and 8 have one set bit each with binary representation "10", "100", and "1000" respectively. The numbers 15 and 30 have four set bits each with binary representation "1111" and "11110".
+        // We can sort the array using 4 operations:
+        // - Swap nums[0] with nums[1]. This operation is valid because 8 and 4 have one set bit each. The array becomes [4,8,2,30,15].
+        // - Swap nums[1] with nums[2]. This operation is valid because 8 and 2 have one set bit each. The array becomes [4,2,8,30,15].
+        // - Swap nums[0] with nums[1]. This operation is valid because 4 and 2 have one set bit each. The array becomes [2,4,8,30,15].
+        // - Swap nums[3] with nums[4]. This operation is valid because 30 and 15 have four set bits each. The array becomes [2,4,8,15,30].
+        // The array has become sorted, hence we return true.
+        // Note that there may be other sequences of operations which also sort the array.
+
+        // Example 2:
+        //
+        // Input: nums = [1,2,3,4,5]
+        // Output: true
+        // Explanation: The array is already sorted, hence we return true.
+
+        // Example 3:
+        //
+        // Input: nums = [3,16,8,4,2]
+        // Output: false
+        // Explanation: It can be shown that it is not possible to sort the input array using any number of operations.
+
+
+        Solution solution = new();
+
+        Console.WriteLine(solution.CanSortArray([8, 4, 2, 30, 15]));
+        Console.WriteLine(solution.CanSortArray([1, 2, 3, 4, 5]));
+        Console.WriteLine(solution.CanSortArray([3, 16, 8, 4, 2]));
+    }
+
+    public bool CanSortArray(int[] nums)
+    {
+        int prevMax = int.MinValue;
+        int currMax = nums[0];
+        int currMin = nums[0];
+        int setBits = Convert.ToString(nums[0], 2).Replace("0", "").Length;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (setBits == Convert.ToString(nums[i], 2).Replace("0", "").Length)
+            {
+                currMax = Math.Max(currMax, nums[i]);
+                currMin = Math.Min(currMin, nums[i]);
+            }
+            else
+            {
+                if (currMin < prevMax) return false;
+
+                prevMax = currMax;
+                setBits = Convert.ToString(nums[i], 2).Replace("0", "").Length;
+                currMin = nums[i];
+                currMax = nums[i];
+            }
+        }
+
+        return currMin > prevMax;
+    }
+}
